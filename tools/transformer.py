44480df60e58fcff46103835d258da631d2151b2
Enginamcis <djerune@gmx.net>
2024-11-21 22:06:55 +0100

    minor update regarding docu

--- tools/transformer.py
+++ tools/transformer.py
@@ -2,18 +2,42 @@ import numpy as np
 from sklearn.base import BaseEstimator, TransformerMixin
 from textblob import TextBlob
 
-# Custom Transformer for Textual Features
 class TextFeatureExtractor(BaseEstimator, TransformerMixin):
+    """
+    Custom transformer for extracting textual features.
+    Extracted features include:
+    - Word count
+    - Character count
+    - Sentiment polarity (using TextBlob)
+    """
     def fit(self, X, y=None):
+        """
+        Fit method. Does nothing as no fitting is required for this transformer.
+        
+        Parameters:
+        - X: Input data.
+        - y: Target labels.
+
+        Returns:
+        - self: The fitted transformer.
+        """
         return self
 
-    def transform(self, X):
-        # Features: word count and sentiment polarity
+    def transform(self, X: np.ndarray) -> np.ndarray:
+        """
+        Transform method to extract features from text.
+        
+        Parameters:
+        - X: Input data
+
+        Returns:
+        - np.ndarray: An array of the extracted features.
+        """
         return np.array([
             [
-                len(text.split()),  # Word count
-                len(text),  # Character count
-                TextBlob(text).sentiment.polarity,  # Sentiment polarity
+                len(text.split()),         # Count words
+                len(text),                 # COunt characters
+                TextBlob(text).sentiment.polarity  # Sentiment polarity
             ]
             for text in X
         ])
